bc51dbd9b0febba8d52b6ef76741c9442cda56de
Josemar Luedke <josemarluedke@gmail.com>
2014-03-30 19:10:38 -0300

    Add admin policy

---
+++ spec/policies/neighborly/admin/admin_policy_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe Neighborly::Admin::AdminPolicy do
+  subject{ Neighborly::Admin::AdminPolicy }
+
+  permissions :access? do
+    it 'should deny access if user is nil' do
+      should_not permit(nil, Neighborly::Admin)
+    end
+
+    it 'should deny access if user is not admin' do
+      should_not permit(User.new, Neighborly::Admin)
+    end
+
+    it 'should permit access if user is admin' do
+      user = User.new
+      user.admin = true
+      should permit(user, Neighborly::Admin)
+    end
+  end
+end
+
