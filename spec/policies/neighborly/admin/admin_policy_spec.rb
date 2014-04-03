0fe9b42498952ca2656e83277098f7de7adc97c0
Josemar Luedke <josemarluedke@gmail.com>
2014-04-03 10:12:28 -0300

    Refactor admin policy to use expect

--- spec/policies/neighborly/admin/admin_policy_spec.rb
+++ spec/policies/neighborly/admin/admin_policy_spec.rb
@@ -4,18 +4,18 @@ describe Neighborly::Admin::AdminPolicy do
   subject { described_class }
 
   permissions :access? do
-    it 'should deny access if user is nil' do
-      should_not permit(nil, Neighborly::Admin)
+    it 'denies access if user is nil' do
+      expect(subject).not_to permit(nil, Neighborly::Admin)
     end
 
-    it 'should deny access if user is not admin' do
-      should_not permit(User.new, Neighborly::Admin)
+    it 'denies access if user is not admin' do
+      expect(subject).not_to permit(User.new, Neighborly::Admin)
     end
 
-    it 'should permit access if user is admin' do
+    it 'permits access if user is admin' do
       user = User.new
       user.admin = true
-      should permit(user, Neighborly::Admin)
+      expect(subject).to permit(user, Neighborly::Admin)
     end
   end
 end
